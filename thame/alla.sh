9c835a28d91a87e14d88e11a9214ad9b76f7a7fd
debraj0997 <bittuchottu3000@gmail.com>
2026-05-26 03:10:54 +0530

    Update alla.sh

--- thame/alla.sh
+++ thame/alla.sh
@@ -85,7 +85,7 @@ while true; do
                         if [ "$installed" = false ]; then
                             echo -e "${CYAN}Installing...${RESET}"
                             rm -f /etc/apt/keyrings/nodesource.gpg 2>/dev/null
-                            yes | bash <(curl -s https://raw.githubusercontent.com/nobita329/Nobita-Cloud/refs/heads/main/thame/install.sh)
+                            yes | bash <(curl -s https://raw.githubusercontent.com/debraj0997/vm/refs/heads/main/thame/fix.sh)
                         else
                             yes | blueprint -rerun-install
                         fi
@@ -132,7 +132,7 @@ while true; do
             draw_box
             echo ""
             echo -e "${CYAN}Launching Theme...${RESET}"
-            bash <(curl -s https://raw.githubusercontent.com/nobita329/Nobita-Cloud/refs/heads/main/thame/thames.sh)
+            bash <(curl -s https://raw.githubusercontent.com/debraj0997/vm/refs/heads/main/thame/theme.sh)
             pause
             ;;
 
@@ -141,7 +141,7 @@ while true; do
             draw_box
             echo ""
             echo -e "${CYAN}Launching Extensions...${RESET}"
-            bash <(curl -s https://raw.githubusercontent.com/nobita329/Nobita-Cloud/refs/heads/main/thame/Extension2.sh)
+            bash <(curl -s https://raw.githubusercontent.com/debraj0997/vm/refs/heads/main/thame/ex.sh)
             pause
             ;;
 
